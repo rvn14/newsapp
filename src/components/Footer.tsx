6c094f62d21cbcdf3e34cf9bf6c2bdd64ce983f2
rvn14 <dasunadithya123@gmail.com>
2025-05-06 16:27:52 +0530

    Footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='bg-red-900 text-white text-xs md:text-md w-screen p-4 flex justify-center items-center'>
+    <footer className='bg-red-900 text-white text-xs md:text-sm w-screen p-4 flex justify-center items-center'>
         Copyright© 2025 NewsApp. All rights reserved.
     </footer>
   )
